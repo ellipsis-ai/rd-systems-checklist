7f4b166d356926b6538efc7160681633300764e9
Andrew Catton <>
2018-07-30 11:18:50 +0000

    Small requested wording change

--- actions/run-room-2-checklist/function.js
+++ actions/run-room-2-checklist/function.js
@@ -2,8 +2,8 @@ function(nutrientTubs, chillers, channels, ellipsis) {
   const support = require('support')(ellipsis);
 
 const questionsAndAnswers = {
-  "Nutrient tubs at least 1/2 full": nutrientTubs,
-  "Chillers running between 70-90 degrees F": chillers
+  "Both nutrient tubs at least 1/2 full": nutrientTubs,
+  "Both chillers running between 70-90 degrees F": chillers
 }
 support.postFor("R & D room 2", questionsAndAnswers, channels);
 }
